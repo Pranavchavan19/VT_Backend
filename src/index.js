42b530eb349b26cb82d6abf2439bb0935c4c4f3c
Pranavbc <pranav1952000@gmail.com>
2024-12-31 23:44:03 +0530

    jj

--- src/index.js
+++ src/index.js
@@ -22,36 +22,23 @@
 
 
 
-// import dotenv from 'dotenv';  // Import dotenv
-// import connectToDB from './db/dbConnect.js';  // Import DB connection function
-// import app from './app.js';  // Import the Express app
+import dotenv from 'dotenv';  // Import dotenv
+import connectToDB from './db/dbConnect.js';  // Import DB connection function
+import app from './app.js';  // Import the Express app
 
-// // Load environment variables from .env
-// dotenv.config({ path: '/env' });
+// Load environment variables from .env
+dotenv.config({ path: '/env' });
 
-// // Connect to the database and start the server
-// connectToDB()
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`Server is listening on: ${process.env.PORT || 8000}`);
-//     });
-// })
-// .catch((error) => console.log("MONGODB connection failed!!!: ", error));
-
-
+// Connect to the database and start the server
+connectToDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening on: ${process.env.PORT || 8000}`);
+    });
+})
+.catch((error) => console.log("MONGODB connection failed!!!: ", error));
 
 
-
-import app from "./app.js"; // Import the Express app
-import http from "http";
-
-const PORT = process.env.PORT || 8000;
-
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
 
 
 
